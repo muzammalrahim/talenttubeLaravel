67690321c08bce35d0bc547f3d64f13e6bdbc1bf
muzammalrahim <creative.joomdev@gmail.com>
2022-03-31 18:46:32 +0500

    job application css fixing

--- resources/views/site/employer/jobApplication.blade.php
+++ resources/views/site/employer/jobApplication.blade.php
@@ -25,29 +25,24 @@
          {{-- ======================================= Filter By Keyword ======================================= --}}
          
          <div class="row">   
-            <div class="col-12 col-sm-4 col-md-4 col-lg-4">
+            <div class="col-md-6 col-sm-6">
                <div class="input-employee clearfix">
                   <div class="row">
                      {{-- <label class="col-md-4">Keyword:</label> --}}
-                     <div class="col-md-4">
-                        <h5 class="browse-heading font-weight-normal">Keyword:</h5>
-                     </div>
-
-                     <input type="text" class="form-control col-md-8" name="filter_keyword">
+                     <label class="col-12 col-sm-3 browse-heading font-weight-normal">Keyword:</label>
+                     <input type="text" class="form-control col-12 col-sm-8 ml-3" name="filter_keyword">
                   </div>
                </div>
             </div>
             
             {{-- ======================================= Filter By Salary ======================================= --}}
 
-            <div class="col-12 col-sm-4 col-md-4 col-lg-4">
+            <div class="col-md-6 col-sm-6">
                <div class="input-employee clearfix">
                   <div class="row">
-                     <div class="col-md-4">
-                        <h5 class="browse-heading font-weight-normal">Salary Range:</h5>
-                     </div>
+                     <label class="col-12 col-sm-3 browse-heading font-weight-normal">Salary:</label>
                      {{-- <input type="text" class="form-control col-md-8" name="filter_salary" aria-label="Recipient's username"> --}}
-                     <select name="filter_salary" class="form-control col-md-8 bg-white" id="filter_salary" data-placeholder="Select Salary Range">
+                     <select name="filter_salary" class="form-control col-12 col-sm-8 ml-3 bg-white icon_show" id="filter_salary" data-placeholder="Select Salary Range">
                         <option value="">Select Salary Range</option>
                         @foreach(getSalariesRange() as $sk => $salary)
                             <option value="{{$sk}}">{{$salary}}</option>
@@ -56,21 +51,21 @@
                   </div>
                </div>
             </div>
+         </div>
 
-            {{-- ======================================= Filter By Sort By ======================================= --}}
+         {{-- ======================================= Filter By Sort By ======================================= --}}
 
-
+         <div class="row">
+           
             {{-- <div class="searchField_sortBy d-flex col-md-4"> --}}
-            <div class="col-12 col-sm-4 col-md-4 col-lg-4">
+            <div class="col-md-6 col-sm-6">
                <div class="input-employee clearfix">
 
                   <div class="row">
-                     <div class="col-md-4">
-                        <h5 class="browse-heading font-weight-normal">Sort By:</h5>
-                     </div>
+                     <label class="col-12 col-sm-3 browse-heading font-weight-normal">Sort By:</label>
 
                      {{-- <div class="sortByFieldLabel col-5 pt-2">Sort By: </div> --}}
-                     <select name="ja_filter_sortBy" class="form-control col-md-8 bg-white">
+                     <select name="ja_filter_sortBy" class="form-control col-12 col-sm-8 ml-3 bg-white icon_show">
                         <option value="goldstars">Gold Stars</option>
                         <option value="applied">Applied</option>
                         <option value="inreview">In Review</option>
@@ -84,22 +79,54 @@
             </div>
 
 
+            {{-- ======================================= Filter By Qualification ======================================= --}}
+
+
+            <div class="col-md-6 col-sm-6">
+               <div class="searchField_qualification b-filter-row">
+                  <div class="row">
+                     {{-- <div class="searchFieldLabel col-5 pt-2">Qualification: </div> --}}  
+                     <label class="col-12 col-sm-3 browse-heading font-weight-normal">Qualification:</label>
+                     <select class="col-12 col-sm-8 ml-3 filter_qualification_type form-control bg-white icon_show" name="ja_filter_qualification_type" data-placeholder="Select Qalification & Trades">
+                        <option value="">Select Qalification & Trades</option>
+                        <option value="certificate">Certificate or Advanced Diploma</option>
+                        <option value="trade">Trade Certificate</option>
+                        <option value="degree">University Degree</option>
+                        <option value="post_degree">University Post Graduate (Masters or PHD)</option>
+                     </select>
+                     @php 
+                        ($qualifications = getQualificationsList())
+                     @endphp
+                     @if(!empty($qualifications))
+                     <div class="filter_qualificaton_degree">
+                        <ul class="qualification_ul item_ul dot_list">
+                           @foreach ($qualifications as $qualif)
+                           <li class="{{$qualif['type']}}" data-id="{{$qualif['id']}}" data-type="ja_filter_qualification[]">
+                              <span>{{$qualif['title']}}</span>
+                           </li>
+                           @endforeach
+                        </ul>
+                     </div>
+                     @endif
+                  </div>
+               </div>
+            </div>
          </div>
 
          {{-- ======================================= Filter By Industry ======================================= --}}
 
 
             <div class="row b-filter-row mt-3">
-               <div class="col-md-12 browse-mp">
+               <div class="col-12 col-md-10 browse-mp">
                   <div class="row">
-                     <div class="col-md-3 col-sm-12 mt-1">
-                        <h5 class="browse-heading font-weight-normal">Filter by Industry:</h5>
+                     <div class="col-3 col-sm-2 mt-1">
+                        <h5 class="browse-heading font-weight-normal">Industry:</h5>
                      </div>
-                     <div class="col-md-1 col-sm-2 col-2 custom-checkbox mt-1">
+                     <div class="col-2 col-sm-1 custom-checkbox mt-1 pl-sm-0">
                         <input type="checkbox" name="filter_industry_status" checked class="">
                      </div>
-                     <div class="col-md-8 col-sm-12 filter_industryList">
-                           <select name="filter_industry[]" multiple="multiple" id="filter_industry" placeholder = "Filter by Job" class="multi-select form-control custom-select ">
+                     <div class="col-sm-9 filter_industryList">
+                           <select name="filter_industry[]" multiple="multiple" id="filter_industry" placeholder = "Filter by Job" class="multi-select form-control custom-select" style="width:100%">
                               @php
                               $industries = getIndustries()
                               @endphp
@@ -111,48 +138,10 @@
                               </div>
                               @endif
                            </select>
+                           <i class="fa fa-angle-down dropdownSelect3 position-absolute"></i>
                            {{-- 
                         </div>
                         --}}
-                     </div>
-                  </div>
-               </div>
-            </div>
-            
-            {{-- ============================================ Filter by Location ============================================ --}}
-
-
-            <div class="row b-filter-row mt-3">
-               <div class="col-md-12">
-                  <div class="row">
-                     <div class="col-md-3 col-sm-12 mt-1 b-mob-pad">
-                        <h5 class="browse-heading font-weight-normal">Filter by Location:</h5>
-                     </div>
-                     <div class="col-1 custom-checkbox mt-1">  
-                        <input type="checkbox" class="" name="filter_location_status">
-                     </div>
-                     <div class="FilterLocationBox col">
-                        <div class="location_search_cont row hide_it">
-                           <div class="col-10 md-form form-sm">
-                              <input type="text" name="location_search" id="location_search" class="form-control form-control-sm white-text"  placeholder="Type a location">
-                           </div>
-                           <div class="col-2">
-                              <select class="white-text mdb-select md-form filter_location_radius custom-select" name="filter_location_radius" data-placeholder="Select Location Radius">
-                                 <option value="5" selected="selected">5km</option>
-                                 <option value="10">10km</option>
-                                 <option value="25">25km</option>
-                                 <option value="50">50km</option>
-                                 <option value="51">50km +</option>
-                              </select>
-                           </div>
-                           <div class="location_latlong d-none w100">
-                              <input type="hidden" class="location_lat w50 fl_left" name="location_lat" id="location_lat" value="" placeholder="Lat" readonly="true" aria-invalid="false">
-                              <input type="hidden" class="location_long w50 fl_left" name="location_long" id="location_long" value="" placeholder="Long" readonly="true" aria-invalid="false">
-                           </div>
-                           <div class="location_map_box dtable w100">
-                              <div class="location_map" id="location_map"></div>
-                           </div>
-                        </div>
                      </div>
                   </div>
                </div>
@@ -162,15 +151,15 @@
 
 
          <div class="row b-filter-row mt-3">
-            <div class="col-md-12">            
+            <div class="col-12 col-md-10">            
                <div class="row">
-                  <div class="col-md-3">  
-                     <h5 class="browse-heading font-weight-normal">Filter by Question:</h5>
+                  <div class="col-3 col-sm-2 mt-1">  
+                     <h5 class="browse-heading font-weight-normal">Question:</h5>
                   </div>
-                  <div class="col-1 custom-checkbox mt-1">
+                  <div class="col-2 col-sm-1 custom-checkbox mt-1 pl-sm-0">
                      <input type="checkbox" class="" name="filter_by_questions">
                   </div>
-                  <div class="col-md-8 questionFilter filter_question_cont">
+                  <div class="col-sm-9 pt-3 pt-sm-0 questionFilter filter_question_cont">
                      @if(varExist('questions', $job))
                      @foreach ($job->questions as $qkey =>  $jq)
                      <div class="jobFilterQuestion row mb-1">
@@ -193,43 +182,45 @@
                </div>
             </div>
          </div>
-         
+            
+            {{-- ============================================ Filter by Location ============================================ --}}
 
-         {{-- ======================================= Filter By Qualification ======================================= --}}
 
-
-         <div class="searchField_qualification row b-filter-row mt-3">
-            <div class="col-md-12">
-               <div class="row">
-                  {{-- <div class="searchFieldLabel col-5 pt-2">Qualification: </div> --}}
-                  <div class="col-md-3">  
-                     <h5 class="browse-heading font-weight-normal">Qualification:</h5>
+            <div class="row b-filter-row mt-3">
+               <div class="col-12 col-md-10">
+                  <div class="row">
+                     <div class="col-3 col-sm-2 mt-1 b-mob-pad">
+                        <h5 class="browse-heading font-weight-normal">Location:</h5>
+                     </div>
+                     <div class="col-2 col-sm-1 custom-checkbox mt-1 pl-sm-0">  
+                        <input type="checkbox" class="" name="filter_location_status">
+                     </div>
+                     <div class="FilterLocationBox col-sm-9 pt-3 pt-sm-0">
+                        <div class="location_search_cont row hide_it">
+                           <div class="col-9 md-form form-sm">
+                              <input type="text" name="location_search" id="location_search" class="form-control form-control-sm white-text"  placeholder="Type a location">
+                           </div>
+                           <div class="col-3">
+                              <select class="white-text mdb-select md-form filter_location_radius custom-select icon_show" name="filter_location_radius" data-placeholder="Select Location Radius">
+                                 <option value="5" selected="selected">5km</option>
+                                 <option value="10">10km</option>
+                                 <option value="25">25km</option>
+                                 <option value="50">50km</option>
+                                 <option value="51">50km +</option>
+                              </select>
+                           </div>
+                           <div class="location_latlong d-none w100">
+                              <input type="hidden" class="location_lat w50 fl_left" name="location_lat" id="location_lat" value="" placeholder="Lat" readonly="true" aria-invalid="false">
+                              <input type="hidden" class="location_long w50 fl_left" name="location_long" id="location_long" value="" placeholder="Long" readonly="true" aria-invalid="false">
+                           </div>
+                           <div class="location_map_box dtable w100">
+                              <div class="location_map" id="location_map"></div>
+                           </div>
+                        </div>
+                     </div>
                   </div>
-
-                  <select class="col-md-9 filter_qualification_type form-control bg-white w-100" name="ja_filter_qualification_type" data-placeholder="Select Qalification & Trades">
-                     <option value="">Select Qalification & Trades</option>
-                     <option value="certificate">Certificate or Advanced Diploma</option>
-                     <option value="trade">Trade Certificate</option>
-                     <option value="degree">University Degree</option>
-                     <option value="post_degree">University Post Graduate (Masters or PHD)</option>
-                  </select>
-                  @php ($qualifications = getQualificationsList())
-                  @if(!empty($qualifications))
-                  <div class="filter_qualificaton_degree">
-                     <ul class="qualification_ul item_ul dot_list">
-                        @foreach ($qualifications as $qualif)
-                        <li class="{{$qualif['type']}}" data-id="{{$qualif['id']}}" data-type="ja_filter_qualification[]">
-                           <span>{{$qualif['title']}}</span>
-                        </li>
-                        @endforeach
-                     </ul>
-                  </div>
-                  @endif
-               </div>   
-
+               </div>
             </div>
-         </div>
-
 
 
          {{-- data-target ="#qualification_modal" data-toggle = "modal" --}}
@@ -615,7 +606,9 @@
                            country = long_name;
                        }
                    }
+
    
+
                    if((city) && (state) && (country))
                        address = city + ", " + state + ", " + country;
                    else if((city) && (state))
@@ -631,7 +624,7 @@
                        // console.log(' reverseGeocode place ', place);
                        // console.log(' reverseGeocode city/state/country = ', city,'/',state,'/',country );
                        updateLocationInputs(place.name,city,state,country);
-                       jQuery("#location_search").val(address);
+                       // jQuery("#location_search").val(address);   // Commented for preventing duplication of location
                        placeMarker(place.geometry.location);
    
                });
@@ -768,7 +761,10 @@
        }
    
        // by default show this location;
+   
        geocode('Sydney New South Wales, Australia');
+   
+   
        jQuery('.filter_location_radius').on('change', function(){
            console.log(' filter_location_radius changed.  ');
            drawCircle(new google.maps.LatLng(jQuery("#location_lat").val(), jQuery("#location_long").val()));
