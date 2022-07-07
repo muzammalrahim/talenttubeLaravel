0ba9e1a5ee1dd00aada203652ca37a0d8244df4c
muzammalrahim <creative.joomdev@gmail.com>
2022-07-07 12:59:11 +0500

    site issues resloved

--- resources/views/web/modals/show_video.blade.php
+++ resources/views/web/modals/show_video.blade.php
@@ -1,4 +1,4 @@
-<div class="modal fade overflow-hidden w-100" id="videoShowModal" tabindex="-1" aria-labelledby="exampleModalLabel" 
+<div class="modal fade" id="videoShowModal" tabindex="-1" aria-labelledby="exampleModalLabel" 
    aria-hidden="true" data-keyboard="false" data-backdrop="static">
   <div class="modal-dialog">
     <div class="modal-content">
@@ -6,7 +6,7 @@
         <h5 class="modal-title" id="videoTitle">Video</h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
-      <div class="modal-body">
+      <div class="modal-body text-center">
 
          <div class="videoBox"></div>
 
