b60e72ba1c7bdb0956388327edca57928e2a0788
muzammalrahim <creative.joomdev@gmail.com>
2022-07-20 15:57:49 +0500

    about page

--- resources/views/site/header/about.blade.php
+++ resources/views/site/header/about.blade.php
@@ -29,13 +29,16 @@
        <div class="row px-3">
          <div class="col-lg-7 col-md-6 col-sm-12 col-12 order-2 order-sm-2 order-2 order-md-1 wow">
            <h3 class="pb-2">About Us</h3>
+
            <p>
              Talent Tube is an all in 1 recruitment solution for connecting great tech talent with employers who are looking for those hidden gems in the job market. Often times we overlook talent due to a bland, ill structured resume, that makes it impossible to showcase the candidate’s soft skills, which when incorrectly assessed, can be attributed to up to    
              <span><a href="#">89% of poor hires</a></span>.   
            </p>
+
            <p>
              At Talent Tube, we don’t pretend to have ‘better’ talent in our database, but offer an opportunity to partner with Employers, and showcase our candidates attributes in a 360 degree view, which includes video introductions, psychometric testing, AI match making algorithms in addition to traditional methods such as CVs and Cover Letters.
            </p>
+           
            <p>
              Speak to one our consultants today, to conduct a short role briefing, and let us help source and showcase suitable candidates for your technical talent needs. Currently we are specialising in developers, UX/UI designers, testers, IT project managers, Dev Ops and more, but also happy to discuss other recruitment needs in the Australian market. 
            </p>
